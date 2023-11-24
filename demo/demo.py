7958d5f7cba6903eda6faef997466f6d7aa27ef3
pc <kakashise@o2.pl>
2023-11-24 12:12:39 +0000

    improved demo

--- demo/demo.py
+++ demo/demo.py
@@ -9,12 +9,18 @@ pygame.display.set_caption('bootleg')
 clock = pygame.time.Clock()
 pixel_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
-snail_surface1 = pygame.image.load('graphics/snail/snail2.png')
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_surface1 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = pixel_font.render('Furiously Fast Snails 2', True, 'Black')
+
+player_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (80,300))
+
+snail_rect = snail_surface.get_rect(midbottom = (600, 320))
+snail_rect1 = snail_surface1.get_rect(midbottom = (600, 370))
 
 snail_x_pos = 600
 snail1_x_pos = 600
@@ -25,20 +31,29 @@ while running:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
-	snail_x_pos -= 1
-	snail1_x_pos -= 2
+	
+	
 	screen.blit(ground_surface,(0,300))
 	screen.blit(sky_surface,(0,0))
 	screen.blit(text_surface,(200, 100))
 	
-	screen.blit(snail_surface,(snail_x_pos, 300))
-	screen.blit(snail_surface1,(snail1_x_pos, 340))
+	snail_rect.left -= 1
+	snail_rect1.left -= 2
+	screen.blit(snail_surface,snail_rect)
+	screen.blit(snail_surface1,snail_rect1)
+
+	screen.blit(player_surface,player_rectangle)
 	
-	if snail_x_pos <= 0:
-		snail_x_pos = 600
-	if snail1_x_pos <= 0:
-		snail1_x_pos = 600
 	
+	
+	if snail_rect.left <= 0:
+		snail_rect.left = 600
+	if snail_rect1.left <= 0:
+		snail_rect1.left = 600
+	
+	if player_rectangle.colliderect(snail_rect):
+		
 	pygame.display.update()
 	clock.tick(60)
+
 
