2230a5aec2731b12fa89d0c63ec7405d3704666d
GRACJAN BLAZEJOWSKI <23017998@uhi.ac.uk>
2023-11-25 13:37:12 +0000

    Effects class. 2 effects completed.

---
+++ effects/effects_class.py
@@ -0,0 +1,14 @@
+
+class Effects:
+    def __init__(self, duration, effect_type, magnitude):
+        print('ini')
+        self.duration = duration
+        self.type = effect_type
+        self.magnitude = magnitude
+
+    def apply(self, target):
+        if self.type == 'heal':
+            target.heal(self.magnitude)
+        elif self.type == 'take_damage':
+            target.take_damage(self.magnitude)
+    '''add additional types as needed'''
