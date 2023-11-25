efa21b08ef20e6bffb650975d26f6868a8645b32
GRACJAN BLAZEJOWSKI <23017998@uhi.ac.uk>
2023-11-25 11:46:32 +0000

    Statistics class for characters

---
+++ stats_class/stats_class.py
@@ -0,0 +1,17 @@
+class CharacterStatistics:
+    def __init__(self, hp, max_hp, mana, max_mana, speed, strength, capacity, gold, experience, level):
+
+        self.hp = hp
+        self.max_hp = max_hp
+        self.mana = mana
+        self.max_mana = max_mana
+
+        self.speed = speed
+        self.strength = strength
+        self.attack = 5 + (strength / 2)
+        self.capacity = capacity
+
+        self.gold = gold
+        self.level = level
+        self.exp = experience
+
