f4e66927209c9b1ac8bf991b35902b02ee43778c
pc <kakashise@o2.pl>
2023-11-29 19:58:48 +0000

    improved demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import pygame
 from sys import exit
 
@@ -13,7 +14,6 @@ class Player(pygame.sprite.Sprite):
 	def animate_player(self):
 
 		global hero_index
-
 		self.hero_index += 0.1
 
 		if self.hero_index >= len(hero_walk):
@@ -43,6 +43,52 @@ class Snail(pygame.sprite.Sprite):
 			if snail.rect.left <= 0:
 				snail.rect.left = 600
 
+class Event_handler:
+	def __init__(self, held_keys, player):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if player.rect.collidepoint(event.pos):
+					print('clicked player')
+				elif snail.rect.collidepoint(event.pos):
+					print('clicked snail')
+					
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_w:
+					held_keys.append('up')
+				elif event.key == pygame.K_s:
+					held_keys.append('down')
+				elif event.key == pygame.K_a:
+					held_keys.append('left')
+				elif event.key == pygame.K_d:
+					held_keys.append('right')
+			elif event.type == pygame.KEYUP:
+				if event.key == pygame.K_w:
+					held_keys.remove('up')
+				elif event.key == pygame.K_s:
+					held_keys.remove('down')
+				elif event.key == pygame.K_a:
+					held_keys.remove('left')
+				elif event.key == pygame.K_d:
+					held_keys.remove('right')
+		self.check_held(held_keys, player)
+
+	def check_held(self, held_keys, player):
+		speed = 4
+		for x in held_keys:
+			if len(held_keys) > 1:
+				speed = 2 
+			if 'up' in held_keys:
+				player.rect.top -= speed
+			if 'left' in held_keys:
+				player.rect.left -= speed
+			if 'right' in held_keys:
+				player.rect.right += speed
+			if 'down' in held_keys:
+				player.rect.bottom += speed
+
 
 screen = pygame.display.set_mode((700, 400))
 pygame.display.set_caption('bootleg')
@@ -54,11 +100,6 @@ hero_surface = pygame.image.load('graphics/Player/player_walk_1.png').convert_al
 hero_surface1 = pygame.image.load('graphics/Player/player_walk_2.png').convert_alpha()
 hero_walk = [hero_surface, hero_surface1]
 
-#snail_move_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-#snail_move_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
-#snail_move = [snail_move_1, snail_move_2]
-#snail_index = 0
-
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 sky_rect = sky_surface.get_rect(topleft=(0,0))
@@ -67,76 +108,27 @@ ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 text_surface = pixel_font.render('Furiously Fast Snails 2', True, 'Black')
 text_rect = text_surface.get_rect(center=(350, 50))
 
-#snail1 = Snail(340)
-#snail2 = Snail(370)
 
 hero_index = 0
 
 player = Player()
-#hero_rect = hero_surface.get_rect(center= (50, 300))
 
-#snail_x_pos = 600
-#snail1_x_pos = 600
 running = True
 held_keys = []
 
 snail = Snail(340)
 
-def check_held(held_keys):
-	global player
-	speed = 4
-	for x in held_keys:
-		if len(held_keys) > 1:
-			speed = 2
-		if 'up' in held_keys:
-			player.rect.top -= speed
-		if 'left' in held_keys:
-			player.rect.left -= speed
-		if 'right' in held_keys:
-			player.rect.right += speed
-		if 'down' in held_keys:
-			player.rect.bottom += speed
-
 while running:
 	condition = True
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			if player.rect.collidepoint(event.pos):
-				print('clicked player')
-			elif snail.rect.collidepoint(event.pos):
-				print('clicked snail')
-				
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_w:
-				held_keys.append('up')
-			elif event.key == pygame.K_s:
-				held_keys.append('down')
-			elif event.key == pygame.K_a:
-				held_keys.append('left')
-			elif event.key == pygame.K_d:
-				held_keys.append('right')
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_w:
-				held_keys.remove('up')
-			elif event.key == pygame.K_s:
-				held_keys.remove('down')
-			elif event.key == pygame.K_a:
-				held_keys.remove('left')
-			elif event.key == pygame.K_d:
-				held_keys.remove('right')
+	event = Event_handler(held_keys, player)
 
-	check_held(held_keys)
+
 	if len(held_keys) > 0:
 		player.animate_player()
 	else:
 		player.surf = hero_walk[0]
 
-	#snail_x_pos -= 1
-	#snail1_x_pos -= 2
-	screen.blit(ground_surface,(0,300))
+	screen.blit(ground_surface,player.rect.center)
 	screen.blit(sky_surface, sky_rect)
 	screen.blit(snail.surf, snail.rect)
 	snail.animate_snail()
@@ -147,7 +139,6 @@ while running:
 		condition = True
 
 	snail.move_snail(condition)
-
 
 	pygame.draw.rect(screen, 'pink', text_rect)
 	screen.blit(text_surface, text_rect)
