cfd035dc4adc6ff7edd465989342f216553f3ec7
GRACJAN BLAZEJOWSKI <23017998@uhi.ac.uk>
2023-12-01 14:13:37 +0000

    item tool: fixed bugs

--- items/item_tool.py
+++ items/item_tool.py
@@ -19,54 +19,64 @@ class ItemCreator:
         self.required_skills = []
 
     def prompt_name(self):
-        self.name = input('Enter item name: ')
-        self.confirm = input(f'{self.name} \n Confirm? y/n	')
-        if self.confirm != 'y' or self.confirm == 'n':
-            self.prompt_name()
-        self.confirm = 'n'
+        while True:
+            self.name = input('Enter item name: ')
+            confirm = input(f'confirm the name of {self.name}? y/n    > ')
+            if confirm != 'y' or confirm == 'n':
+                continue
+            elif confirm == 'y':
+                break
 
     def prompt_weight(self):
-        self.weight = input('Enter item weight: ')
-        try:
-            self.weight = float(self.weight)
-            self.confirm = input(f'{self.weight} \n Confirm? y/n	')
-            if self.confirm != 'y' or self.confirm == 'n':
-                self.prompt_weight()
-
-        except ValueError:
-            self.prompt_weight()
-        self.confirm = 'n'
+        while True:
+            self.weight = input('Enter item weight: ')
+            try:
+                self.weight = float(self.weight)
+                confirm = input(f'confirm the weight of {self.weight}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
+            except ValueError:
+                continue
 
     def prompt_value(self):
-        self.value = input('Enter base value: ')
-        try:
-            self.value = int(self.value)
-            self.confirm = None
-            self.confirm = input(f'\n{self.value}\nConfirm? y/n	')
-            if self.confirm != 'y' or self.confirm == 'n':
-                print(self.confirm)
-                self.prompt_value()
+        while True:
+            self.value = input('Enter base value: ')
+            try:
+                self.value = int(self.value)
+                confirm = input(f'confirm the value of {self.value}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
 
-        except ValueError:
-            self.prompt_value()
-        self.confirm = 'n'
-
+            except ValueError:
+                continue
+            
+            
     def prompt_durability(self):
-        self.durability = input('Enter max durability (default is 1.0): ')
-        try:
-            self.durability = float(self.durability)
-            self.confirm = input(f'\n{self.durability}\nConfirm? y/n	')
-            if self.confirm == 'n' or self.confirm != 'y':
-                self.prompt_durability()
+        while True:
+            self.durability = input('Enter max durability (default is 1.0)    > ')
+            try:
+                self.durability = float(self.durability)
+                confirm = input(f'confirm the durability of {self.durability}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
 
-        except ValueError:
-            self.prompt_durability()
+            except ValueError:
+                continue
 
     def prompt_description(self):
-        self.description = input('Enter item description: ')
-        confirm = input(f'{self.description}\n Confirm? y/n	')
-        if confirm != 'y' or confirm == 'n':
-            self.prompt_description()
+        while True:
+            self.description = input('Enter item description: ')
+            confirm = input(f'confirm the following description?\n\n{self.description}\n y/n    > ')
+            if confirm != 'y' or confirm == 'n':
+                continue
+            elif confirm == 'y':
+                break
 
     def prompt_min_lvl(self):
         self.min_lvl = input('Enter minimum level to use the item: ')
@@ -115,15 +125,26 @@ class ItemCreator:
         return
 
     def create_file(self):
-        filename = input('Enter filename: ')
-        confirm = input(f'save changes to {filename}.json? y/n	')
-        if confirm == 'y':
-            filepath = os.path.join(filename + '.json')
-            with open(filepath, 'w') as new_file:
-                json.dump(self.final_dict, new_file)
-
-        elif confirm == 'n' or confirm != 'y':
-            self.create_file()
+        while True:
+            filename = input('Enter filename: ')
+            specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', ':', ';', '[', ']', '\'', '\\', ',', '<', '>', '.', '/', '?',
+                        '   ', '`', '~', '-', '=', '+']
+            illegal_chars = []
+            for char in filename:
+                if char in specials:
+                    print(f'illegal special character: {char}')
+                    illegal_chars += char
+            if illegal_chars:
+                print('resetting ...')
+                continue
+            else:
+                confirm = input(f'save changes to {filename}.json? y/n    > ')
+                if confirm == 'y':
+                    filepath = os.path.join(filename + '.json')
+                    with open(filepath, 'w') as new_file:
+                        json.dump(self.final_dict, new_file)
+                elif confirm == 'n' or confirm != 'y':
+                    continue
 
     def prompt_damage_modifier(self):
 
@@ -132,7 +153,7 @@ class ItemCreator:
 
             try:
                 self.dmd = float(self.dmd)
-                confirm = input(f'{self.dmd} \n Confirm? y/n	')
+                confirm = input(f'confirm the damage modifier of {self.dmd}? y/n    > ')
                 if confirm != 'y' or confirm == 'n':
                     continue
                 elif confirm == 'y':
@@ -145,18 +166,26 @@ class ItemCreator:
 
     def prompt_damage(self):
         while True:
-            min_dmg = input('enter minimum damage 	> ')
+            min_dmg = input('enter minimum damage    > ')
             try:
                 min_dmg = int(min_dmg)
-                break
+                confirm = input(f'confirm min damage of {min_dmg}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
         while True:
-            max_dmg = input('enter maximum damage 	> ')
+            max_dmg = input('enter maximum damage    > ')
             try:
                 max_dmg = int(max_dmg)
-                break
+                confirm = input(f'confirm max damage of {max_dmg}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
@@ -164,10 +193,14 @@ class ItemCreator:
 
     def prompt_attack_speed(self):
         while True:
-            a_s = input('Enter attack speed 	> ')
+            a_s = input('Enter attack speed    > ')
             try:
                 a_s = float(a_s)
-                break
+                confirm = input(f'confirm the attack speed of {a_s}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
@@ -175,10 +208,14 @@ class ItemCreator:
 
     def prompt_range(self):
         while True:
-            range = input('Enter range 	> ')
+            range = input('Enter range    > ')
             try:
                 range = float(range)
-                break
+                confirm = input(f'confirm the range of {range}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
@@ -186,10 +223,14 @@ class ItemCreator:
 
     def prompt_defense(self):
         while True:
-            defense = input('Enter defense 	> ')
+            defense = input('Enter defense    > ')
             try:
                 defense = int(defense)
-                break
+                confirm = input(f'confirm the defense of {defense}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
@@ -197,13 +238,17 @@ class ItemCreator:
 
     def prompt_hands(self):
         while True:
-            hands = input('How many hands to hold the item? 1/2	> ')
+            hands = input('How many hands to hold the item? (1/2)    > ')
             try:
                 hands = int(hands)
                 if hands != 1 and hands != 2:
                     continue
                 else:
-                    break
+                    confirm = input(f'confirm the hands of {hands}? y/n    > ')
+                    if confirm != 'y' or confirm == 'n':
+                        continue
+                    elif confirm == 'y':
+                        break
             except ValueError:
                 continue
 
@@ -211,20 +256,28 @@ class ItemCreator:
 
     def prompt_crit_chance(self):
         while True:
-            crit_chance = input('Enter crit chance (float) 		> ')
+            crit_chance = input('Enter crit chance    > ')
             try:
                 crit_chance = float(crit_chance)
-                break
+                confirm = input(f'confirm the crit chance of {crit_chance}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
         self.crit_chance = crit_chance
 
     def prompt_speed_modifier(self):
         while True:
-            speed_mod = input('Enter speed modifier 	> ')
+            speed_mod = input('Enter speed modifier    > ')
             try:
                 speed_mod = float(speed_mod)
-                break
+                confirm = input(f'confirm the speed modifier of {speed_mod}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
@@ -233,14 +286,18 @@ class ItemCreator:
     def prompt_weapon_type(self):
         types = {1: 'melee', 2: 'ranged', 3: 'magic'}
         while True:
-            self.type = input('Select one of the following weapon types:\n1: melee		2: ranged	3: magic')
+            self.type = input('Select one of the following weapon types:\n1: melee    2: ranged    3: magic')
             try:
                 self.type = int(self.type)
                 if int(self.type) != 1 and int(self.type) != 2 and int(self.type) != 3:
                     continue
                 else:
                     self.type = types[int(self.type)]
-                    break
+                    confirm = input(f'confirm the weapon type of {self.type}? y/n    > ')
+                    if confirm != 'y' or confirm == 'n':
+                        continue
+                    elif confirm == 'y':
+                        break
             except ValueError:
                 continue
 
@@ -248,7 +305,7 @@ class ItemCreator:
         types = {1: 'helmet', 2: 'chest', 3: 'legs', 4: 'boots', 5: 'shield'}
         while True:
             self.type = input(
-                'Select one of the following armor types:\n1: helmet		2: chest	3: legs\n4: boots 	5: shield')
+                'Select one of the following armor types:\n1: helmet    2: chest    3: legs\n4: boots    5: shield')
             try:
                 self.type = int(self.type)
                 if int(self.type) != 1 and int(self.type) != 2 and int(self.type) != 3 and int(self.type) != 4 and int(
@@ -256,7 +313,11 @@ class ItemCreator:
                     continue
                 else:
                     self.type = types[int(self.type)]
-                    break
+                    confirm = input(f'confirm the armor type of {self.type}? y/n    > ')
+                    if confirm != 'y' or confirm == 'n':
+                        continue
+                    elif confirm == 'y':
+                        break
             except ValueError:
                 continue
 
@@ -265,7 +326,7 @@ class ItemCreator:
                  8: 'trap'}
         while True:
             self.type = input(
-                'Select one of the following tool types:\n1: pickaxe		2: shovel 		3: hoe\n4: rope		5: watering can		6: scythe\n7: fishing rod		8: trap')
+                'Select one of the following tool types:\n1: pickaxe    2: shovel    3: hoe\n4: rope    5: watering can    6: scythe\n7: fishing rod    8: trap')
             try:
                 self.type = int(self.type)
                 if int(self.type) != 1 and int(self.type) != 2 and int(self.type) != 3 and int(self.type) != 4 and int(
@@ -273,7 +334,11 @@ class ItemCreator:
                     continue
                 else:
                     self.type = types[int(self.type)]
-                    break
+                    confirm = input(f'confirm the tool type of {self.type}? y/n    > ')
+                    if confirm != 'y' or confirm == 'n':
+                        continue
+                    elif confirm == 'y':
+                        break
             except ValueError:
                 continue
 
@@ -281,7 +346,7 @@ class ItemCreator:
         while True:
             classes = {1: 'weapon', 2: 'armor', 3: 'accessory', 4: 'tool', 5: 'consumable', 6: 'quest item'}
             self.category = input(
-                'select one of the following item classes:\n1: weapon	2: armor	3: accessory\n4: tool	5: consumable	6: quest item\n\n')
+                'select one of the following item classes:\n1: weapon    2: armor    3: accessory\n4: tool    5: consumable    6: quest item\n\n')
             try:
                 self.category = int(self.category)
             except ValueError:
@@ -292,15 +357,23 @@ class ItemCreator:
                 continue
             if int(self.category) in list(classes.keys()):
                 self.category = classes[self.category]
-                break
+                confirm = input(f'confirm category of {self.category}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
 
 
     def prompt_duration(self):
         while True:
             self.duration = input('Enter duration in seconds    > ')
             try:
-                self.duration = int(self.quantity)
-                break
+                self.duration = int(self.duration)
+                confirm = input(f'Confirm duration of {self.duration}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
@@ -310,7 +383,11 @@ class ItemCreator:
             self.quantity = input('Enter maximum number of item in stack    > ')
             try:
                 self.quantity = int(self.quantity)
-                break
+                confirm = input(f'Confirm quantity of {self.quantity}? y/n    > ')
+                if confirm != 'y' or confirm == 'n':
+                    continue
+                elif confirm == 'y':
+                    break
             except ValueError:
                 continue
 
