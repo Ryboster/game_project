297e5c99d27364a3e7d7c3f4a9434ed973d5d552
Bogdan Todorow <andrewbagio@yahoo.com>
2023-12-03 21:06:32 +0000

    mapp adding prototype

--- main/main.py
+++ main/main.py
@@ -4,35 +4,56 @@ from pygame.locals import *
 pygame.init()
 pygame.display.set_caption('game base')
 screen = pygame.display.set_mode((900,900),0,32)
-display = pygame.Surface((15000,15000))
 
-water_img = pygame.image.load('.\water_tile.png').convert()
+surface_size=(500,500)
+sS = surface_size
+
+display = pygame.Surface((sS[0],sS[1]))
+
+water_img = pygame.image.load('./tilesets/water_64.png').convert()
+land_img = pygame.image.load('./tilesets/land_64.png').convert()
+
 water_img.set_colorkey((0,0,0))
+land_img.set_colorkey((0,0,0))
+Two_D_array = [["Water", "Water", "Water", "Water","Water","Water"],
+["Water", "Land", "Land", "Land", "Water","Water"],
+["Water", "Land", "Land", "Land", "Water","Water"],
+["Water", "Land", "Land", "Land", "Land","Water"],
+["Water", "Land", "Land", "Land", "Land","Water"],
+["Water", "Land", "Land", "Land", "Land","Water"],
+["Water", "Land", "Land", "Land", "Water","Water"],
+["Water", "Land", "Land", "Land", "Water","Water"],
+["Water", "Water", "Land", "Water","Water","Water"],]
+
+
+def display_w(x,y):
+    display.blit(water_img, (x,y))
+
+def display_l(x,y):
+    display.blit(land_img, (x,y))
+
+function_mapping = {'Water': display_w, 'Land': display_l}
+
 
 while True:
     display.fill((0,0,0))
-    x = 7500
-    y = 100
-    y_value= y
-    x_value = x
-    map_size =40
-    y_move_total = 120 *map_size
-    for y_value in range(100, y_move_total , 120):
-        for x_value in range(x, -1, -205):
-        #for _ in range(map_size):
-                display.blit(water_img, (x_value,y_value))
-                x_value -= 205
-                y_value+=120
+    x = sS[0]//3
+    y = 10
+    temp_x = x
+    temp_y = y
+    current_x_lvl = x
+    current_y_lvl = y
+    for row in Two_D_array: 
+        for element in row:
+            function_mapping[element](temp_x,temp_y)
+            #display.blit(water_img, (temp_x,temp_y))
+            temp_x-=32
+            temp_y+=16
+        current_x_lvl += 32
+        current_y_lvl += 16
+        temp_x =  current_x_lvl
+        temp_y =   current_y_lvl
 
-        y +=y_value+120
-        x += 205
-       # print(value)
-
-    #display.blit(water_img, (x,y))
-    #display.blit(water_img, (x-205,y+120))
-    # display.blit(water_img, (x-205-205,y+120+120))
-    # display.blit(water_img, (150+205,100+117))
-    # display.blit(water_img, (150+205+205,100+117+117))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
