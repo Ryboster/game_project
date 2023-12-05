72bdd9a0d1d27cee7ac91844def6313491b718ca
pc <kakashise@o2.pl>
2023-12-05 19:46:37 +0000

    test

--- demo/render_concept/test/test.py
+++ demo/render_concept/test/test.py
@@ -1,12 +1,16 @@
 import pygame, sys
 import csv
-
-with open('map.csv', 'r') as file:
-    csv_file = csv.reader(file)
-    for row in csv_file:
-        print(row)
+import numpy as np
 
 pygame.init()
+
+file = open('map.csv', 'r')
+csv_file = csv.reader(file)
+
+#with open('map.csv', 'r') as file:
+#    csv_file = csv.reader(file)
+
+csv2_file = csv.reader(file)
 
 screen = pygame.display.set_mode((1280, 720))
 width, height = screen.get_size()
@@ -64,9 +68,19 @@ class Player(pygame.sprite.Sprite):
             self.rect.center += (self.direction / 1.5) * self.speed
         
 
+class Water(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.surf = pygame.image.load('water_tile.png').convert_alpha()
+        self.surf = pygame.transform.scale(self.surf, (64,64))
+        self.rect = self.surf.get_rect(topleft=pos)
+        self.surf.set_colorkey((0,0,0))
+
+
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
+        
         self.display_surf = pygame.display.get_surface()
         
         # Camera offset
@@ -75,38 +89,21 @@ class CameraGroup(pygame.sprite.Group):
         self.half_y = self.display_surf.get_size()[1] // 2
         self.wh = self.display_surf.get_size()
         
-
         # Camera rect
         self.camera_rect = pygame.Rect(self.offset.x, self.offset.y, self.half_w, self.half_y)
         
         ### Zoom
-        self.zoom_level = 1.0 
+        self.zoom_level = 1.0
+        self.myArray = [['w','w','w','w','w'],['w','w','w','w','w'],['w','w','w','w','w'],['w','w','w','w','w'],['w','w','w','w','w']]
+        k = np.array(self.myArray)
+
         
     def chunk_world(self, world, player):
-        self.world = world
-        
+        self.world = world   
         player_pos = (int(world[0] // 64), int(world[1] // 64))
-        
-        
+
         visible_tiles = []
-        
-        
-        #print(self.world.x // 64, self.world.y // 64)
-         
-        #if self.world.x < 0:
-        #    player_pos[0] = 0
-        #else:
-        #    player_pos[0] = self.world[0]
-        #    #self.world.x = player.rect.center[0]
-        
-       # if self.world.y < 0:
-        #    player_pos[1] = 0
-        #else:
-        #    self.world.y = player.rect.center[1]
-        
-        
-        
-        
+
     def box_camera(self, target):
         self.offset.x = target.rect.centerx - self.half_w
         self.offset.y = target.rect.centery - self.half_y
@@ -119,25 +116,95 @@ class CameraGroup(pygame.sprite.Group):
         
         # Box camera
         self.box_camera(player)
-        
+
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            if isinstance(sprite, type(player)):
+                pass
+
+            player_pos = self.get_xy(player)
+            
+            player_offset = sprite.rect.topleft - self.offset
+            #player_pos = player.rect.center
+            sprite_pos = self.get_xy(sprite)
+
+            #print(sprite_pos)
+            #print(player_pos)
+
+            #if sprite_pos == player_pos:
+            #    screen.blit(sprite.surf, player_offset)
+                #print((sprite.rect.x // 64))
+
+                #print(self.myArray[(sprite.rect.x // 64) - 1: (sprite.rect.x // 64) + 2 ])
+
+        #    [x-1: x+2,  y-1: y+2]
+        #    if sprite.rect.x 
+
             player_offset = sprite.rect.topleft - self.offset
             self.display_surf.blit(sprite.surf, player_offset)
+
+        player_offset = player.rect.topleft - self.offset
+        self.display_surf.blit(player.surf, player_offset)
+        #print(player.rect.x // 64, player.rect.y //64)
+
+    
+    def get_xy(self, target):
+
+        return (target.rect.x // 64,
+                target.rect.y //64)
+    
+    def create_map(self, k,Water,player):
+        results = self.get_xy(player)
+        print(results[0],results[1])
+        
+        x= results[0]
+        if (x <= 0):
+            x=1
+
+        y = results[1]
+        #print(y)
+        if(y<=0):
+            y=1
+        #[int(y-1): int((x+2)), int(y-1): (y+2)]    
+        #print(k[1:4, 1:4])
+        print( k ) # (y-1): (y+2)] ) 
+        #print(k[(x-1): (x+2), (y-1): (y+2)])
+        #print(k,k[x-1: x+2])
+        new_k= k[int(y-1): int((x+2)), int(y-1): (y+2)]
+        x = 0
+        y = 0
+        for row in new_k:
+            for col in row:
+                if col == 'w':
+                    #Water((x,y), camera_group)
+                    self.add(Water((x,y)))
+                x += 64
+            x = 0
+            y += 64
+      
 
 
 camera_group = CameraGroup()
 player = Player((0,0), camera_group)
 
+myArray = [['w','w','w','w','w'],['w','w','w','w','w'],['w','w','w','w','w'],['w','w','w','w','w'],['w','w','w','w','w']]
+k = np.array(myArray)
+#print(camera_group.get_xy(player,k))
+
+
+
 
 while True:
+    screen.fill((0,0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                 pygame.image.save(screen,'map.png')
                 pygame.quit()
                 sys.exit()
                 
-    camera_group.custom_draw(player)
+    camera_group.create_map(k,Water,player)
     camera_group.chunk_world(world, player)
+    camera_group.custom_draw(player)
+    
     camera_group.update()
-    #screen.fill((255,255,255))
+    
     pygame.display.update()
