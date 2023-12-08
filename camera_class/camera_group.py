a2fe549dde00cc6abfa981fd7c65fdbd845fb645
GRACJAN BLAZEJOWSKI <23017998@uhi.ac.uk>
2023-12-08 14:58:27 +0000

    T-55: Add and utilize offset

---
+++ camera_class/camera_group.py
@@ -0,0 +1,16 @@
+import pygame
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init()
+        # Get display surface
+        self.display_surf = pygame.display.get_surface()
+
+        # Camera offset
+        self.offset = pygame.math.Vector2()
+        self.half_W = self.display_surf.get_size()[0] // 2
+        self.half_y = self.display_surf.get_size()[1] // 2
+        self.screen_size = self.display_surf.get_size()
+        
+        # Camera Rect
+        self.camera_rect = pygame.Rect(self.offset.x, self.offset.y, self.screen_size[0], self.screen_size[1])
