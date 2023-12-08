b675838586adf12efc2218a3665489666d4b6869
GRACJAN BLAZEJOWSKI <23017998@uhi.ac.uk>
2023-12-08 15:47:13 +0000

    T-54: Create a target box camera

---
+++ camera_class/camera_group.py
@@ -0,0 +1,32 @@
+import pygame
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init()
+        # Get display surface
+        self.display_surf = pygame.display.get_surface()
+
+        # Camera offset
+        self.offset = pygame.math.Vector2()
+        self.half_W = self.display_surf.get_size()[0] // 2
+        self.half_y = self.display_surf.get_size()[1] // 2
+        self.screen_size = self.display_surf.get_size()
+
+        # Camera Rect
+        self.camera_rect = pygame.Rect(self.offset.x, self.offset.y, self.screen_size[0], self.screen_size[1])
+
+
+    def center_target(self, target):
+        self.offset.x = target.rect.centerx - self.half_W
+        self.offset.y = target.rect.centery - self.half_y
+
+        self.camera_rect.centerx = target.rect.centerx - self.offset.x
+        self.camera_rect.centery = target.rect.centery - self.offset.y
+        self.camera_rect.center = target.rect.center
+        
+    def ysort(self, player):
+        self.center_target(player)
+        
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surf.blit(sprite, offset_pos)
