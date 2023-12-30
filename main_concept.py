d5cf9fa22a6f2ce630245349f795f2fa42ef69fd
Bogdan Todorow <andrewbagio@yahoo.com>
2023-12-30 12:18:47 +0000

    player movement function prints presed key

--- main_concept.py
+++ main_concept.py
@@ -1,4 +1,4 @@
-#from map import load_map
+# from map import load_map
 import pygame, sys
 import csv
 import numpy as np
@@ -6,59 +6,66 @@ from character_class.main_character import Player as player
 from ui.user_interface import UserInterface
 from effects.effects_class import mouseEvents
 
-#twoDArray = []
 twoDArr = []
-with open('map/alpha_map.csv', newline='') as file:
+with open("map/alpha_map.csv", newline="") as file:
     twoDArr = np.array(list(csv.reader(file)))
 
 print(twoDArr)
 pygame.init()
 screen = pygame.display.set_mode((1024, 720))
-sizeTouple = screen.get_rect(topleft=(0,0))
+sizeTouple = screen.get_rect(topleft=(0, 0))
 
-def createSprite(name,fileName,position,width=0, height=0):
 
+def createSprite(name, fileName, position, width=0, height=0):
     name = pygame.sprite.Sprite()
-    # if filename == "player.png":
-    #     name.image = pygame.image.load(f'')
-    # else:
-    name.image = pygame.image.load(f'{fileName}.png').convert_alpha()
+    name.image = pygame.image.load(f"{fileName}.png").convert_alpha()
     name.rect = name.image.get_rect(topleft=position)
-    
+
     if width and height:
-        name.image = pygame.transform.scale(name.image,(width,height))
+        name.image = pygame.transform.scale(name.image, (width, height))
 
-    return  name
+    return name
 
-water = createSprite("water","./images/water_tile",(0,0),64,64)
-ground = createSprite("ground","./images/ground_tile",(0,0),64,64)
+
+water = createSprite("water", "./images/water_tile", (0, 0), 64, 64)
+ground = createSprite("ground", "./images/ground_tile", (0, 0), 64, 64)
+player = createSprite(
+    "player",
+    "./images/player",
+    (screen.get_size()[0] // 2, screen.get_size()[1] // 2),
+    32,
+    32,
+)
 
 
 def cut_piece_from_TwoD_Arr(coordTuple, map_data, renderedSize):
     if coordTuple[0] <= 0:
-        width = 0    
+        width = 0
     else:
         width = coordTuple[0]
     if coordTuple[1] <= 0:
-        height  = 0
+        height = 0
     else:
         height = coordTuple[1]
 
-    current_row = height // 18
+    current_row = height // 16
     current_column = width // 32
 
-    return map_data[current_row: current_row + renderedSize,
-                     current_column: current_column + renderedSize]
+    return map_data[
+        current_row : current_row + renderedSize,
+        current_column : current_column + renderedSize,
+    ]
 
 
-visibleArr = cut_piece_from_TwoD_Arr(screen.get_rect().topleft, twoDArr, 20)   
-           
+visibleArr = cut_piece_from_TwoD_Arr(screen.get_rect().topleft, twoDArr, 20)
+
 
 bg_tiles_Group = pygame.sprite.Group()
 
-def display_bg_tiles(x=0,y=0,tilesize=64):
-    print(screen.get_rect().midtop)
-    start_x =screen.get_rect().midtop[0]
+
+def display_bg_tiles(x=0, y=0, tilesize=64):
+    # print(screen.get_rect().midtop)
+    start_x = screen.get_rect().midtop[0]
     start_y = screen.get_rect().midtop[1]
     rowNum = start_x
     columnNum = start_y
@@ -66,23 +73,34 @@ def display_bg_tiles(x=0,y=0,tilesize=64):
     column = 1
     for rows in visibleArr:
         for element in rows:
-            screen.blit(ground.image,(rowNum,columnNum))
-            bg_tiles_Group.add(createSprite("water","./images/water_tile",(rowNum,columnNum),64,64))
-            rowNum -= tilesize/2### Move to the left
-            columnNum += tilesize/4 ### Move to the bottom
+            screen.blit(ground.image, (rowNum, columnNum))
+            bg_tiles_Group.add(
+                createSprite("water", "./images/water_64", (rowNum, columnNum), 64, 64)
+            )
+            rowNum -= tilesize / 2  ### Move to the left
+            columnNum += tilesize / 4  ### Move to the bottom
             print(row)
 
-        
-        #add new line start coordinates
-        rowNum = start_x +((tilesize/2)*row)
-        columnNum =  start_y+ ((tilesize/4)*column)
+        # add new line start coordinates
+        rowNum = start_x + ((tilesize / 2) * row)
+        columnNum = start_y + ((tilesize / 4) * column)
         column += 1
         row += 1
 
-        ###
 
 display_bg_tiles()
-        ###
+def player_movement(event):
+
+    if event.key == 119 or event.key == 1073741906:
+        print("event type up")
+    elif event.key == 100 or event.key == 1073741903:
+        print("event type right")
+    elif event.key == 115 or event.key == 1073741905:
+        print("event type down")
+    elif event.key == 97 or event.key == 1073741904:
+        print("event type left")
+
+###
 # player = createSprite("player",
 #                       "demo/graphics/player",
 #                       (screen.get_size()[0] // 2,
@@ -98,60 +116,60 @@ display_bg_tiles()
 #     left = keys[pygame.K_LEFT]
 #     right = keys[pygame.K_RIGHT]
 #     if up:
-     
+
 #         direction.y = -1
 #     elif down:
 #         direction.y = 1
 #     elif not up and not down and speed > 0.1 and (left or right):
-#         direction.y = 0  
-                    
+#         direction.y = 0
+
 #     if right:
 #         direction.x = 1
 #     elif left:
 #         direction.x = -1
 #     elif not right and not left and speed > 0.1 and (up or down):
 #         direction.x = 0
-        
+
 #     if not up and not down and not right and not left:
 #         speed /= 1.3
 #         if speed <= 0.1:
 #             direction.x = 0
 #             direction.y = 0
-           
-            
+
+
 # def input(event):
 #         keys = pygame.key.get_pressed()
 #         up = keys[pygame.K_UP]
 #         down = keys[pygame.K_DOWN]
 #         right = keys[pygame.K_RIGHT]
 #         left = keys[pygame.K_LEFT]
-         
+
 #         if up:
 #             direction.y = -1
 #         elif down:
 #             direction.y = 1
 #         elif not up and not down and speed > 0.1 and (left or right):
-#             direction.y = 0  
-                        
+#             direction.y = 0
+
 #         if right:
 #             direction.x = 1
 #         elif left:
 #             direction.x = -1
 #         elif not right and not left and speed > 0.1 and (up or down):
 #             direction.x = 0
-            
+
 #         if not up and not down and not right and not left:
 #             speed /= 1.3
 #             if speed <= 0.1:
 #                 direction.x = 0
 #                 direction.y = 0
-            
+
 # def update:
 #         #input()
-        
+
 #         speed += acceleration
 #         speed = min(speed, max_speed)
-        
+
 #         if direction.x and not direction.y:
 #             rect.center += direction * speed
 #         elif direction.y and not direction.x:
@@ -159,20 +177,14 @@ display_bg_tiles()
 #         elif direction.y and direction.x:
 #             rect.center += (direction / 1.5) * speed
 
-'''
+"""
 ARRR[1:4, 1:4]
 
-'''
+"""
 
-ui =  UserInterface(screen)
-#camera_group = camera_group()
-#player = player((screen.get_size()[0] // 2, screen.get_size()[0] // 2), camera_group)
-
+ui = UserInterface(screen)
 clock = pygame.time.Clock()
 
-
-
-        
 running = True
 mesh_toggle = False
 
@@ -189,24 +201,12 @@ while running:
                 else:
                     mesh_toggle = False
                     pygame.time.delay(100)
-                    
-            # if event.key == pygame.K_EQUALS or event.key == pygame.K_PLUS:
-            #     camera_group.zoom_level += 0.1
-            #     pygame.time.delay(30)
-            # elif event.key == pygame.K_MINUS:
-            #     camera_group.zoom_level -= 0.1
-            #     pygame.time.delay(30)
-
+            player_movement(event)
         elif event.type == pygame.MOUSEBUTTONUP:
             mouseEvents(ui)
 
-
-    screen.fill((0,0,0))
-    #display_bg_tiles()
-   # screen.blit(player.image,player.rect)
-    #ui.display_ui()
+    screen.fill((0, 0, 0))
+    bg_tiles_Group.draw(screen)
+    screen.blit(player.image, player.rect)
     pygame.display.update()
     clock.tick(60)
-
-
-
