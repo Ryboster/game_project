76c21d07d110aeaa376b95583b6f0269093f4f3e
Bogdan Todorow <andrewbagio@yahoo.com>
2024-01-06 07:23:47 +0000

    first version of movement created

--- main_concept.py
+++ main_concept.py
@@ -44,27 +44,30 @@ player = createSprite(
 def cut_piece_from_TwoD_Arr(coords, map_data, size):
     coords_x = coords[0]
     coords_y = coords[1]
-    start_row = 0
-    start_column= 0
-    end_row= coords[0]+(size // 2)
-    end_column= coords[1]+(size // 2)
-    # if visible area for the left side of the player do not fall behind array length
-    # set start row half the lenght of the visible size from the player
-    # otherwise set start row to 0
-    if coords_x - (size // 2) >= 0:      
-        start_row = coords_x - (size // 2)
-    else:
-        start_row = 1
+    start_row = coords_x
+    end_row= coords_y
+    start_column=  coords_x +size 
+    end_column= coords_y+ size 
+    # # if visible area for the left side of the player do not fall behind array length
+    # # set start row half the lenght of the visible size from the player
+    # # otherwise set start row to 0
+    # if coords_x - (size // 2) >= 0:      
+    #     start_row = coords_x - size 
+    # else:
+    #     start_row = 1
+    #     start_column =size
         
-    if coords_y - (size // 2) >= 0:
-        start_column = coords_y - (size // 2)
-    else:
-       start_column = 1
+    # if coords_y - (size // 2) >= 0:
+    #     start_column = coords_y - (size // 2)
+    # else:
+    #    end_row = 0
+    #    #start_column = 0
+    #    end_column = size
     #arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
-    testarr = np.array([[ 0,  1,  2,  3],[ 4  ,5,  6,  7],[ 8,  9, 10, 11],[12, 13, 14, 15],[16, 17 ,18, 19],[20, 21, 22, 23]])
+    #testarr = np.array([[ 0,  1,  2,  3],[ 4  ,5,  6,  7],[ 8,  9, 10, 11],[12, 13, 14, 15],[16, 17 ,18, 19],[20, 21, 22, 23]])
     '''debugging prints:'''    
-    print(start_row,start_column,end_row,end_column,coords)
-    print(map_data[start_row:end_row, end_row:end_column])
+    print(start_row,start_column,end_row,end_column,coords,size)
+    print(map_data[start_row:start_column, end_row:end_column])
     # print(map_data[
     #     start_row: start_column,
     #     end_row: end_column,
@@ -73,7 +76,7 @@ def cut_piece_from_TwoD_Arr(coords, map_data, size):
     #     start_row: start_column,
     #     end_row: end_column,
     # ]
-    return map_data[start_row:end_row, 1:8]
+    return map_data[start_row:start_column, end_row:end_column]
 
 
 visibleArr = cut_piece_from_TwoD_Arr((0,0), twoDArr, 5)
@@ -111,17 +114,17 @@ class plyer_movement():
         super().__init__()
         self.coordinates_x = 0
         self.coordinates_y = 0
-        
-    
+        self.isometric_position_x = 0
+        self.isometric_position_y = 0
     def player_movement(self,keys):
         if keys[pygame.K_UP]:
             self.coordinates_y += 5
         elif keys[pygame.K_DOWN]:
             self.coordinates_y -= 5
         elif keys[pygame.K_RIGHT]:
-            self.coordinates_x -= 5
-        elif keys[pygame.K_LEFT]:
             self.coordinates_x += 5
+        elif keys[pygame.K_LEFT]:
+            self.coordinates_x -= 5
 
 
 newPlayer = plyer_movement()
@@ -138,7 +141,10 @@ def create_isometric_coordinates(cartesian_x, cartesian_y):
     coordy= (2*ytest-xtest)//(ground.image.get_rect().width )
     return (coordx,coordy)
 
-
+restartx= 0
+#screen.get_size()[0] 
+restarty =  50
+#screen.get_size()[1] //4
 current_player_pos = (0,0)
 
 
@@ -147,8 +153,11 @@ while running:
     if current_player_pos != create_isometric_coordinates(newPlayer.coordinates_x,newPlayer.coordinates_y):
         current_player_pos = create_isometric_coordinates(newPlayer.coordinates_x,newPlayer.coordinates_y)
         visibleArr = cut_piece_from_TwoD_Arr(current_player_pos, twoDArr, 6)
-
-    
+        #newPlayer.coordinates_x = screen.get_size()[1] // 2
+        #newPlayer.coordinates_y = screen.get_size()[0] // 2
+        restartx= 0
+        print(restartx,restarty)
+        #restarty =  (newPlayer.coordinates_y+ screen.get_size()[1] // 2)
     #this should limit allowed events to read  
     pygame.event.set_allowed(None)
     pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP])
@@ -174,7 +183,7 @@ while running:
     
     #print("x and y coord",create_isometric_coordinates(newPlayer.coordinates_x,newPlayer.coordinates_y))
 
-    quicker_set_bg_tiles(newPlayer.coordinates_x, newPlayer.coordinates_y)
+    quicker_set_bg_tiles(restartx,restarty)
 
     screen.blit(player.image, player.rect)
     pygame.display.update()
